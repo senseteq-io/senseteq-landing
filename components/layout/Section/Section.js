f59d5dadf75d454a67faff92756d29af0d131e84
olenashevchuk <olenashevchuk0@gmail.com>
2022-06-15 18:04:46 +0300

    [FIX] updated tag section to class name #done

--- components/layout/Section/Section.js
+++ components/layout/Section/Section.js
@@ -1,8 +1,16 @@
+import { getClassNames } from '../../../utils'
+
 const Section = (props) => {
   const { children, className } = props
 
+  // [COMPUTED PROPERTY]
+  const classNames = getClassNames({
+    section: true,
+    [className]: className
+  })
+
   return (
-    <section className={className}>
+    <section className={classNames}>
       <div className="section-content">{children}</div>
     </section>
   )
