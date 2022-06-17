a575696aa346611fc4e9c47fcaa1aadad6400854
olenashevchuk <olenashevchuk0@gmail.com>
2022-06-17 14:08:55 +0300

    [LAYOUT] [SSQ-3253] created thurd section for Main page #done

--- pages/index.js
+++ pages/index.js
@@ -36,10 +36,30 @@ const HOW_WE_DO_IT_ITEMS = [
       'Vi har et bibliotek av egenutviklede moduler som vi raskt kan integrere i nye prosjekter. Å endre dem til nye applikasjoners behov går fort og vi slipper å utvikle funksjonaliteten fra grunn.'
   }
 ]
+
+const SERVICES_ITEMS = [
+  {
+    title: 'Komplekse back-end applikasjoner',
+    icon: '/complex_backend_app.svg'
+  },
+  {
+    title: 'Front-end web applikasjoner',
+    icon: '/front_end_web_apps.svg'
+  },
+  { title: 'Mobilapplikasjoner', icon: '/mobile_apps.svg' },
+  {
+    title: 'Invividuell teknologikonsultasjon',
+    icon: '/custom_technology.svg'
+  },
+  { title: 'Utvikling av prototype', icon: '/prototyping.svg' },
+  { title: 'UI/UX design', icon: '/ui_ux_design.svg' },
+  { title: 'Prosjektstyring', icon: '/project_managment.svg' }
+]
+
 export default function Home() {
   return (
     <PageWrapper className="main-page">
-      <Section className="first-section">
+      <Section id="prime-section">
         <div className="row">
           <div className="col-12 col-lg-8 col-xl-10">
             <div className="title-wrapper">
@@ -51,14 +71,14 @@ export default function Home() {
             </div>
           </div>
           <div className="col-12 col-lg-8">
-            <p>
+            <p className="subtitle">
               Tenker du at det er umulig? Les videre for å se hvordan vi jobber.
             </p>
           </div>
         </div>
 
         <div className="down-arrow-wrapper">
-          <Link href="/#how-we-do-it">
+          <Link href="/#how-we-do-it-section">
             <a className="down-arrow-icon">
               <Image
                 src="/down_arrow.svg"
@@ -70,7 +90,7 @@ export default function Home() {
           </Link>
         </div>
       </Section>
-      <Section id="how-we-do-it" className="second-section">
+      <Section id="how-we-do-it-section">
         <div className="row">
           <div className="col-12">
             <h2>
@@ -83,10 +103,34 @@ export default function Home() {
           {HOW_WE_DO_IT_ITEMS?.map(({ title, description }) => (
             <div
               key={title}
-              className="how-we-do-it-item-wrapper col-12 col-lg-4">
+              className="how-we-do-it-item-wrapper col-12 col-xl-6 col-xxl-4">
               <div className="how-we-do-it-item">
-                <h5 className="title">{title}</h5>
+                <h3 className="title">{title}</h3>
                 <p className="description">{description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </Section>
+      <Section id="services-section">
+        <div className="row">
+          <div className="col-12">
+            <h2>
+              <span className="regular">Våre</span> tjenester
+              <span className="c-primary">.</span>
+            </h2>
+          </div>
+        </div>
+        <div className="row">
+          {SERVICES_ITEMS?.map(({ icon, title }) => (
+            <div
+              key={title}
+              className="service-item-wrapper col-12 col-lg-4 col-xl-">
+              <div className="service-item">
+                <div className="service-icon-wrapper">
+                  <Image src={icon} alt={title} layout="fill" />
+                </div>
+                <p className="caption">{title}</p>
               </div>
             </div>
           ))}
