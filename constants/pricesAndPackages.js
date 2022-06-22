6ef43dd4b665e1d86423d9b62972213ac616afe2
olenashevchuk <olenashevchuk0@gmail.com>
2022-06-22 12:38:09 +0300

    [LAYOUT] [SSSQ-3304] created pricing page #done

---
+++ constants/pricesAndPackages.js
@@ -0,0 +1,28 @@
+const PRICES_AND_PACKAGES = [
+  {
+    name: 'Liten',
+    price: '50.000,-',
+    version: 'begrenset',
+    examples: 'blogg.no'
+  },
+  {
+    name: 'Medium',
+    price: '100.000,-',
+    version: 'medium',
+    examples: 'CanEat'
+  },
+  {
+    name: 'Stor',
+    price: '300.000,-',
+    version: 'stort',
+    examples: 'Finn.no, Booking.com, Getaraound'
+  },
+  {
+    name: 'Omfattende',
+    price: '600.000,-',
+    version: 'omfattende',
+    examples: 'Foodora, Wolt, Über'
+  }
+]
+
+export default PRICES_AND_PACKAGES
