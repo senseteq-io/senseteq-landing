79fe095419984592dd5eed14f77214f54297ba28
Oleksiy Pastukhov <alexpassss@gmail.com>
2022-06-29 22:34:10 +0300

    [GRAPH META TAGS] Added twitter graph meta tags to the header #done

--- modules/calculator/components/global/Head.js
+++ modules/calculator/components/global/Head.js
@@ -24,6 +24,13 @@ export default function PageHead({ title, description, image }) {
       <meta property="og:type" content="website" />
       <meta property="og:description" content={description} />
       <meta property="og:image:alt" content="Senseteq" />
+
+      {/* <!-- Twitter --> */}
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:image" content={image} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@website-username" />
     </Head>
   )
 }
