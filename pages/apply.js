ff91f23b7cfece55f59831f55eeb1646a03ffc4b
olenashevchuk <olenashevchuk0@gmail.com>
2022-07-02 00:15:18 +0300

    [TRANSLATIONS] [SSQ-3700] added translations for apply page #done

--- pages/apply.js
+++ pages/apply.js
@@ -1,18 +1,29 @@
 import { PageWrapper, Section } from '../components'
 
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from 'next-i18next'
 
 export default function Apply() {
+  // [ADDITIONAL_HOOKS]
+  /* A hook that allows us to use the `t` function to translate strings. */
+  const { t } = useTranslation('landing')
+
   return (
-    <PageWrapper className="apply-page">
+    <PageWrapper
+      className="apply-page"
+      pageMetaProps={{
+        title: t('head.apply.title')
+      }}>
       <Section dark id="prime-section">
         <div className="row">
           <div className="col-12">
             <div className="title-wrapper">
-              <h2 data-aos="fade-down">
-                Se om vi matcher
-                <span className="c-primary">.</span>
-              </h2>
+              <h2
+                data-aos="fade-down"
+                dangerouslySetInnerHTML={{
+                  __html: t('apply.prime_section.title')
+                }}
+              />
             </div>
           </div>
         </div>
