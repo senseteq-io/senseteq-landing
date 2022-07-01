ab26a1f572203fc5d65f6c47b3642925edb54a21
olenashevchuk <olenashevchuk0@gmail.com>
2022-07-01 23:59:37 +0300

    [TRANSLATIONS] [SSQ-3698] added translations for team page #done

--- constants/team.js
+++ constants/team.js
@@ -2,72 +2,72 @@ const TEAM = {
   EXPERIENCED: [
     {
       name: 'Trond Klæboe',
-      role: 'Daglig leder',
+      role: 'constants.roles_items.ceo',
       src: '/team/Trond_Kleboe.webp'
     },
     {
       name: 'Oleksiy Pastukhov',
-      role: 'Senior Full stack utvikler / Team lead',
+      role: 'constants.roles_items.senior_full_stack_and_team_lead',
       src: '/team/Oleksiy_Pastukhov.webp'
     },
     {
       name: 'Oleg Makhynia',
-      role: 'Senior Backend utvikler',
+      role: 'constants.roles_items.senior_backend',
       src: '/team/Oleg_Makhynia.webp'
     },
     {
       name: 'Evgeniy Bogdanov',
-      role: 'Designer UI/UX',
+      role: 'constants.roles_items..designer',
       src: '/team/Evgeniy_Bogdanov.webp'
     },
     {
       name: 'Arthur Pochinok',
-      role: 'Full stack utvikler',
+      role: 'constants.roles_items.full_stask',
       src: '/team/Arthur_Pochinok.webp'
     },
     {
       name: 'Vlad Bogach',
-      role: 'Full stack utvikler',
+      role: 'constants.roles_items.full_stask',
       src: '/team/Vlad_Bogach.webp'
     },
     {
       name: 'Slava Shamrelyuk',
-      role: 'Full stack utvikler',
+      role: 'constants.roles_items.full_stask',
       src: '/team/Slava_Shamrelyuk.webp'
     },
     {
       name: 'Maxim Makarov',
-      role: 'Full stack utvikler',
+      role: 'constants.roles_items.full_stask',
       src: '/team/Maxim_Makarov.webp'
     },
     {
       name: 'Rostyslav Marchuk',
-      role: 'Full stack utvikler',
+      role: 'constants.roles_items.full_stask',
       src: '/team/Rostyslav_Marchuk.webp'
     },
     {
       name: 'Olena Shevchuk',
-      role: 'Full stack utvikler',
+      role: 'constants.roles_items.full_stask',
       src: '/team/Olena_Shevchuk.webp'
     },
     {
       name: 'Ruslana Halkina',
-      role: 'Full stack utvikler',
+      role: 'constants.roles_items.full_stask',
       src: '/team/Ruslana_Halkina.webp'
     },
     {
       name: 'Oleksandr Holyk',
-      role: 'Full stack utvikler',
+      role: 'constants.roles_items.full_stask',
       src: '/team/Oleksandr_Holyk.webp'
     },
     {
       name: 'Dmytro Okrushko',
-      role: 'Full stack utvikler',
+      role: 'constants.roles_items.full_stask',
       src: '/team/Dmytro_Okrushko.webp'
     },
     {
       name: 'Oleksandr Kazhuro',
-      role: 'Full stack utvikler',
+      role: 'constants.roles_items.full_stask',
       src: '/team/Oleksandr_Kazhuro.webp'
     }
   ],
@@ -75,37 +75,32 @@ const TEAM = {
     {
       name: 'Anhelina Bohdanova',
       src: '/team/Anhelina_Bohdanova.webp',
-      role: 'Junior Fullstack utvikler'
+      role: 'constants.roles_items.junior'
     },
     {
       name: 'Oleksandr Mushtyn',
       src: '/team/Oleksandr_Mushtyn.webp',
-      role: 'Junior Fullstack utvikler'
+      role: 'constants.roles_items.junior'
     },
     {
       name: 'Anastasia Shapovalova',
       src: '/team/Anastasia_Shapovalova.webp',
-      role: 'Junior Fullstack utvikler'
+      role: 'constants.roles_items.junior'
     },
     {
       name: 'Oleksandr Voichyshyn',
       src: '/team/Oleksandr_Voichyshyn.webp',
-      role: 'Junior Fullstack utvikler'
+      role: 'constants.roles_items.junior'
     },
     {
       name: 'Andrii Myrovitskyi',
       src: '/team/Andrii_Myrovitskyi.webp',
-      role: 'Junior Fullstack utvikler'
-    },
-    {
-      name: 'Daniil Sheremeta',
-      src: '/team/Daniil_Sheremeta.webp',
-      role: 'Junior Fullstack utvikler'
+      role: 'constants.roles_items.junior'
     },
     {
       name: 'Artem Sukhiy',
       src: '/team/Artem_Sukhiy.webp',
-      role: 'Junior Fullstack utvikler'
+      role: 'constants.roles_items.junior'
     }
   ]
 }
--- pages/team.js
+++ pages/team.js
@@ -2,11 +2,15 @@ import { PageWrapper, Section } from '../components'
 
 import { TEAM } from '../constants'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from 'next-i18next'
 
 export default function Team() {
-  const { RISING_STARS, EXPERIENCED } = TEAM
+  // [ADDITIONAL_HOOKS]
+  /* A hook that allows us to use the `t` function to translate strings. */
+  const { t } = useTranslation('landing')
 
   // [COMPUTED_PROPERTIES]
+  const { RISING_STARS, EXPERIENCED } = TEAM
   const sectionsConfig = [
     { title: 'Hvem er vi?', idSection: 'prime-section' },
     { title: 'Kommende stjerner', idSection: 'rising-stars-section' }
@@ -16,29 +20,25 @@ export default function Team() {
     <PageWrapper
       className="team-page"
       pageMetaProps={{
-        title: 'Team',
-        description:
-          'Vi er et sammensveiset team som stortrives på jobb. Vi feirer hverandres fremgang og hjelper når det trengs.',
-        keywords: 'team, utviklere'
+        title: t('head.team.title'),
+        description: t('head.team.description'),
+        keywords: t('head.team.keywords')
       }}
       sectionsConfig={sectionsConfig}>
       <Section dark id="prime-section">
         <div className="row">
           <div className="col-12">
             <div className="title-wrapper">
-              <h2>
-                Hvem er vi
-                <span className="c-primary">?</span>
-              </h2>
+              <h2
+                dangerouslySetInnerHTML={{
+                  __html: t('team.prime_section.title')
+                }}
+              />
             </div>
           </div>
           <div className="col-12">
             <p className="subtitle-wrapper">
-              Vi er et sammensveiset team som stortrives på jobb. Vi feirer
-              hverandres fremgang og hjelper når det trengs. Vi har en flat
-              struktur i selskapet, bygget på tillit og kompetanse. Alle våre
-              utviklere er engasjerte og føler raskt eierskap til nye
-              prosjekter.
+              {t('team.prime_section.subtitle')}
             </p>
           </div>
         </div>
@@ -56,7 +56,7 @@ export default function Team() {
                   }}
                 />
                 <p className="name">{name}</p>
-                <p className="role">{role}</p>
+                <p className="role">{t(role)}</p>
               </div>
             </div>
           ))}
@@ -65,18 +65,16 @@ export default function Team() {
       <Section id="rising-stars-section">
         <div className="row">
           <div className="col-12">
-            <h2 data-aos="fade-down">
-              Våre kommende stjerner
-              <span className="c-primary">.</span>
-            </h2>
+            <h2
+              data-aos="fade-down"
+              dangerouslySetInnerHTML={{
+                __html: t('team.rising_stars_section.title')
+              }}
+            />
           </div>
           <div className="col-12">
             <p className="subtitle-wrapper" data-aos="fade-left">
-              Jeg må innrømme det, vi ser på dem som stjerner allerede. De er
-              dedikerte, lidenskapelige og de elsker å utvikle produkter som
-              betyr noe. Gled deg til å lytte til deres idéer og bli inspirert
-              av deres innspill. Og selvfølgelig: De leverer fantastisk kode,
-              som bygger fantastiske produkter.{' '}
+              {t('team.rising_stars_section.subtitle')}
             </p>
           </div>
         </div>
@@ -94,7 +92,7 @@ export default function Team() {
                   }}
                 />
                 <p className="name">{name}</p>
-                <p className="role">{role}</p>
+                <p className="role">{t(role)}</p>
               </div>
             </div>
           ))}
