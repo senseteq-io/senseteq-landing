9135615fd7bcaa54ce0f915d43139b9a8de307a2
olenashevchuk <olenashevchuk0@gmail.com>
2022-07-02 00:10:24 +0300

    [TRANSLATIONS] [SSQ-3699] added translations for contact page #done

--- pages/contact.js
+++ pages/contact.js
@@ -3,22 +3,27 @@ import { PageWrapper, Section } from '../components'
 import { CONTACTS_ITEMS } from '../constants'
 import Image from 'next/image'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from 'next-i18next'
 
 export default function Contact() {
+  // [ADDITIONAL_HOOKS]
+  /* A hook that allows us to use the `t` function to translate strings. */
+  const { t } = useTranslation('landing')
+
   return (
     <PageWrapper
       className="contact-page"
       pageMetaProps={{
-        title: 'Kontact',
-        keywords: 'kontact'
+        title: t('head.contact.title')
       }}>
       <Section dark id="prime-section">
         <div className="row">
           <div className="col-12">
-            <h2>
-              Snakk med oss
-              <span className="c-primary">.</span>
-            </h2>
+            <h2
+              dangerouslySetInnerHTML={{
+                __html: t('contact.prime_section.title')
+              }}
+            />
           </div>
         </div>
       </Section>
@@ -33,7 +38,7 @@ export default function Contact() {
                 <div className="contact-image-wrapper">
                   <Image src={src} alt={name} layout="fill" />
                 </div>
-                <p className="caption">{name}</p>
+                <p className="caption">{t(name)}</p>
                 {details?.map((detail) => (
                   <p key={detail} className="body2">
                     {detail}
