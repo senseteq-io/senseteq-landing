a85f001cb0c75c08ebbf5254d85bcdd08a23a47d
olenashevchuk <olenashevchuk0@gmail.com>
2022-07-05 14:40:52 +0300

    [REFACTOR] Refacrored Title component #done

--- components/elements/Typography/Title/Title.js
+++ components/elements/Typography/Title/Title.js
@@ -8,22 +8,17 @@ const DEFAULT_TAG = 'h1'
  * It returns a React element
  * @param props - This is the props object that is passed to the component.
  * @returns A React component that renders an HTML element with the tag name specified by the `as`
- * prop, or `h1` if no `as` prop is provided. The component's class name is set to the tag name, and
- * the component's inner HTML is set to the value of the `text` prop.
+ * prop, or `h1` if no `as` prop is provided. The component's class name is set to the tag name.
  */
 export default function Title(props) {
-  const { as, children, text } = props
+  const { as, children } = props
 
   const tag = as || DEFAULT_TAG
 
-  return createElement(
-    tag,
-    {
-      className: cn({
-        [styles[tag]]: true
-      }),
-      dangerouslySetInnerHTML: { __html: text }
-    },
-    children
-  )
+  return createElement(tag, {
+    className: cn({
+      [styles[tag]]: true
+    }),
+    dangerouslySetInnerHTML: { __html: children }
+  })
 }
