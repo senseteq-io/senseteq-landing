b7b4d07cb5e351a7172abe9922357f8c3848375f
olenashevchuk <olenashevchuk0@gmail.com>
2022-07-06 12:12:44 +0300

    [REFACTOR] [SWE-520] Implemented List component to ServiceList component #done

--- domains/Service/components/ServiceList/ServiceList.js
+++ domains/Service/components/ServiceList/ServiceList.js
@@ -1,23 +1,14 @@
+import { List } from '../../../../components'
 import { SERVICES } from '../../__constants__'
 import ServiceView from '../../components/ServiceView'
-import cn from 'classnames'
-import styles from '../../Service.module.css'
 
 const ServiceList = () => {
-  // [COMPUTED PROPERTIES]
-  const className = cn({ [styles['service-item-wrapper']]: true })
-
   return (
-    <div className="row">
-      {SERVICES?.map(({ src, title }) => (
-        <div
-          data-aos="fade-left"
-          key={title}
-          className={`${className} col-12 col-md-6 col-lg-4`}>
-          <ServiceView title={title} src={src} />
-        </div>
-      ))}
-    </div>
+    <List
+      data={SERVICES}
+      listItemComponent={ServiceView}
+      listItemWrapperClassName="col-12 col-md-6 col-lg-4 service-item-wrapper"
+    />
   )
   return
 }
