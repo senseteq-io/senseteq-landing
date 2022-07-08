fa19344e7a6f39dbe5b96825c10e990d7a06daf3
olenashevchuk <olenashevchuk0@gmail.com>
2022-07-08 15:37:32 +0300

    [SCHEMA ATTRIBUTES] [SWE-705] Added Schema attributes to technologies page #done

--- domains/Technology/components/TechnologyView/TechnologyView.js
+++ domains/Technology/components/TechnologyView/TechnologyView.js
@@ -5,11 +5,16 @@ const TechnologyView = (props) => {
   const { name, src } = props
 
   return (
-    <div className="technology-item">
+    <div
+      className="technology-item"
+      itemScope
+      itemType="https://schema.org/Offer">
       <div className="technology-image-wrapper">
         <Image src={src} alt={name} layout="fill" />
       </div>
-      <Title as="h4">{name}</Title>
+      <Title as="h4" itemprop="name">
+        {name}
+      </Title>
     </div>
   )
 }
