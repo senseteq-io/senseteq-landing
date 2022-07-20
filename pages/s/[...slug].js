ce31b3cb438200ec34eb1c14fd2bbefffac142d7
Oleksiy Pastukhov <alexpassss@gmail.com>
2022-07-20 21:24:42 +0300

    [FEATURE] Added pipedrive scripts to the calculator page #done

--- pages/s/[...slug].js
+++ pages/s/[...slug].js
@@ -10,6 +10,7 @@ import {
 } from '../../modules/calculator/hooks'
 
 import { CalculatorProvider } from '../../modules/calculator/contexts/Calculator'
+import Script from 'next/script'
 import ls from '../../modules/calculator/utils/ls'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useEffect } from 'react'
@@ -85,6 +86,42 @@ export default function CalculatorAll() {
           {page}
         </CalculatorProvider>
       </Content>
+      <Script
+        id="pipedriveLeadboosterConfig"
+        dangerouslySetInnerHTML={{
+          __html: `window.pipedriveLeadboosterConfig = {
+        base: 'leadbooster-chat.pipedrive.com',
+        companyId: 10552104,playbookUuid: 'f467b8c0-118f-4e92-90de-213f5ac7c5e9',
+        version: 2};(function () {var w = window;if (w.LeadBooster) {console.warn('LeadBooster already exists');} else {w.LeadBooster = {q: [],on: function (n, h) {this.q.push({ t: 'o', n: n, h: h });},trigger: function (n) {this.q.push({ t: 't', n: n });},};}})();`
+        }}
+      />
+      <Script
+        src="https://leadbooster-chat.pipedrive.com/assets/loader.js"
+        async
+      />
+      <Script
+        id="function"
+        dangerouslySetInnerHTML={{
+          __html: `(function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('JMvZ8g0A90z72pOd'); `
+        }}
+      />
+      <Script
+        id="VIDEOASK_EMBED_CONFIG"
+        dangerouslySetInnerHTML={{
+          __html: `window.VIDEOASK_EMBED_CONFIG = {
+      "kind": "widget",
+      "url": "https://www.videoask.com/fxat2tdbz",
+      "options": {
+        "widgetType": "VideoThumbnailSmall",
+        "text": "",
+        "backgroundColor": "#60DE74",
+        "position": "bottom-left",
+        "dismissable": true
+      }
+    }`
+        }}
+      />
+      <Script src="https://www.videoask.com/embed/embed.js" async />
       {/* <Footer /> */}
     </>
   )
