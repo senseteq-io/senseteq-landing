2f25f4bbb7efb715d87a7e2afd0f32c87f47dd9d
GingerBoy14 <maks.27.04.2002@gmail.com>
2022-08-09 13:00:08 +0300

    SWE-2083 Change option styles #done

--- modules/calculator/domains/Question/components/Content/Content.js
+++ modules/calculator/domains/Question/components/Content/Content.js
@@ -1,5 +1,7 @@
+import { useRouter } from 'next/router'
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai'
 import Navigation from '../Navigation'
-import { OptionList } from '../../../../components'
+import { Button, OptionList } from '../../../../components'
 import Title from '../Title'
 import styles from './Content.module.css'
 import { useCalculator } from '../../../../contexts/Calculator'
@@ -11,19 +13,40 @@ import { useCalculator } from '../../../../contexts/Calculator'
 export default function Content({ title, subtitle, options, type }) {
   /* Destructuring the `onSelect`, `onNext`, and `onPrev` functions from the `useCalculator` hook. */
   const { onSelect, onNext, onPrev, calculatorData } = useCalculator()
-
+  const router = useRouter()
   /* Returning the content of the page. */
   return (
     <div className={styles.fadeIn}>
       <Title title={title} subtitle={subtitle} />
-      <OptionList
-        className="mb-3"
-        type={type}
-        options={options}
-        onSelect={onSelect}
-        calculatorData={calculatorData}
-      />
-      <Navigation onPrev={onPrev} onNext={onNext} />
+      <div className="container">
+        <div className="row">
+          <div className="col d-none d-sm-flex justify-content-end align-items-center mb-3">
+            <Button
+              variant="outlined"
+              shape="circle"
+              onClick={() => router.back()}>
+              <AiOutlineArrowLeft />
+            </Button>
+          </div>
+          <div className="col-12 col-md-9">
+            <OptionList
+              className="mb-3"
+              type={type}
+              options={options}
+              onSelect={onSelect}
+              calculatorData={calculatorData}
+            />
+          </div>
+          <div className="col d-none d-sm-flex justify-content-start align-items-center  mb-3">
+            <Button variant="outlined" shape="circle" onClick={onNext}>
+              <AiOutlineArrowRight />
+            </Button>
+          </div>
+        </div>
+        <div className="d-block d-sm-none">
+          <Navigation onPrev={onPrev} onNext={onNext} />
+        </div>
+      </div>
     </div>
   )
 }
