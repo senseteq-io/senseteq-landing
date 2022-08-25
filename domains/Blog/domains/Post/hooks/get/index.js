9008872043713270c4af85ed2c75cb18e22fed21
VladBogach <bogach.vladislav@gmail.com>
2022-08-25 15:07:58 +0300

    removed useless hook

--- domains/Blog/domains/Post/hooks/get/index.js
+++ domains/Blog/domains/Post/hooks/get/index.js
@@ -1,2 +1,1 @@
 export { default as useGetPosts } from './useGetPosts'
-export { default as useGetRecommendedPosts } from './useGetRecommendedPosts'
--- domains/Blog/domains/Post/hooks/get/useGetRecommendedPosts/index.js
+++
@@ -1,3 +0,0 @@
-import useGetRecommendedPosts from './useGetRecommendedPosts'
-
-export default useGetRecommendedPosts
