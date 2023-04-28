25fd6b374ba23d029ad8466dad1e6caff70fec1b
Oleksiy Pastukhov <alexpassss@gmail.com>
2023-04-28 17:21:26 +0300

    Replaced clarity script

--- components/layout/PageWrapper/PageWrapper.js
+++ components/layout/PageWrapper/PageWrapper.js
@@ -75,17 +75,6 @@ const PageWrapper = (props) => {
         }}
       />
       <Script
-        id="clarity"
-        dangerouslySetInnerHTML={{
-          __html: `(function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "gvme9s7uss");
-          `
-        }}
-      />
-      <Script
         id="VIDEOASK_EMBED_CONFIG"
         dangerouslySetInnerHTML={{
           __html: `window.VIDEOASK_EMBED_CONFIG = {
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,23 @@
 import '../styles/globals.css'
 
 import { Analytics } from '@vercel/analytics/react'
+import Script from 'next/script'
 import { appWithTranslation } from 'next-i18next'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        id="clarity"
+        dangerouslySetInnerHTML={{
+          __html: `(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "gvme9s7uss");
+          `
+        }}
+      />
       <Component {...pageProps} />
       <Analytics />
     </>
