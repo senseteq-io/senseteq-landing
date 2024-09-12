5406b5537a374536f0f7563a8f5374b542b892e0
Oleksandr Holyk <aleksandr.golyk@gmail.com>
2024-09-12 17:01:01 +0200

    [MINOR] hidden phone, added email redirect

--- components/layout/Footer/Footer.js
+++ components/layout/Footer/Footer.js
@@ -1,11 +1,12 @@
-import { IconButton, Text, Title } from '../../../components'
+import { Text, Title } from "../../../components";
 
-import { useTranslation } from 'next-i18next'
+import Link from "next/link";
+import { useTranslation } from "next-i18next";
 
 const Footer = () => {
   // [ADDITIONAL_HOOKS]
   /* A hook that allows us to use the `t` function to translate strings. */
-  const { t } = useTranslation('landing')
+  const { t } = useTranslation("landing");
 
   return (
     <footer className="footer">
@@ -13,34 +14,38 @@ const Footer = () => {
         <div className="row">
           <div className="col-12 subtitle-wrapper">
             <Text caption className="subtitle">
-              {t('footer.subtitle')}
+              {t("footer.subtitle")}
             </Text>
           </div>
         </div>
         <div className="col-12 title-wrapper">
           <Title as="h4" dataAos="fade-down">
-            {t('footer.title')}
+            {t("footer.title")}
           </Title>
         </div>
         <div className="col-12 contacts-wrapper">
-          <div className="contact buttons-wrapper">
+          {/* <div className="contact buttons-wrapper">
             <IconButton
               href="https://www.facebook.com/senseteq"
               target="_blank"
               src="/assets/facebook.webp"
               alt="Facebook"
-              ariaLabel={t('footer.facebookAria')}
+              ariaLabel={t("footer.facebookAria")}
             />
             <IconButton
               href="tel:+4755181900"
               src="/assets/whatsApp.png"
               alt="WhatsApp"
-              ariaLabel={t('footer.whatsAppAria')}
+              ariaLabel={t("footer.whatsAppAria")}
             />
-          </div>
-          <Text ariaLabel={t('footer.emailAria')} className="contact">
+          </div> */}
+          <Link
+            href="mailto:hello@senseteq.io"
+            ariaLabel={t("footer.emailAria")}
+            className="contact"
+          >
             hello@senseteq.io
-          </Text>
+          </Link>
           {/* Temporary hidden */}
           {/* <Text ariaLabel={t('footer.phoneAria')} className="contact">
             +47 55 18 19 00
@@ -48,7 +53,7 @@ const Footer = () => {
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
